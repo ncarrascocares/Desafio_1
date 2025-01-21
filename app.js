84c11fe955e861dc67b19391c085f0849f5dd47b
ncarrascocares <nicolascarrasco15@gmail.com>
2025-01-21 17:18:51 -0300

    Se agregan las funcionalidad en el js

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = "Hora del Desafío."
 
 function mensaje(){
-    alert("El boton fue clicado");
+    console.log("El boton fue clicado");
+}
+
+function preguntarCiudad(){
+    ciudad = prompt(alert("Ingresa una ciudad de Brasil: "))
+    alert(`Estuve en ${ciudad} y me acordé de ti`);
+}
+
+function mensajeAlert(){
+    alert("Yo amo JS");
+}
+
+function mensajeSuma(){
+    primerNumero = parseInt(prompt(alert("Ingresa el primer número")));
+    segundoNumero = parseInt(prompt(alert("Ingresa el segundo número")));
+    suma = primerNumero+segundoNumero;
+    alert(`La suma de los números ingresados es: ${suma}`);
 }
